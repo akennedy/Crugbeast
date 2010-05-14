ac6d7a5a07ed29fb78ad714681881d82d7d8be02
Andrew Kennedy <andrew@jackrussellsoftware.com>
2010-05-13 22:08:37 -0400

    update mailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -14,8 +14,8 @@ class UserMailer < ActionMailer::Base
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "CrugBeast"
+      @subject     = "CrugBeast "
       @sent_on     = Time.now
       @body[:user] = user
     end
